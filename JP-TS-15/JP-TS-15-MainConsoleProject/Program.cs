4539424b63dd8c5a5f6d235f77868f1dd5a3eb8a
Nikoloz Chkhartishvili <nika.chkhartishvili7@gmail.com>
2023-05-18 19:55:24 +0400

    Properties added

--- JP-TS-15/JP-TS-15-MainConsoleProject/Program.cs
+++ JP-TS-15/JP-TS-15-MainConsoleProject/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using JP_TS_15_MainConsoleProject;
+
+
+User user = new()
+{
+    FirstName = "Nika",
+    LastName = "Chkhartishvili",
+    Pin = "12345678945",
+    Account = new Account()
+    {
+        AccountNumber = "1234567894512345678945",
+        Balance = 100.2m,
+        Currency = "GEL"
+    }
+};
+
+
+Console.WriteLine($"{user.FirstName} {user.LastName} {user.Pin} {user.Account.AccountNumber} {user.Account.Currency} {user.Account.Balance}");
+
---
+++ JP-TS-15/JP-TS-15-MainConsoleProject/User.cs
@@ -0,0 +1,24 @@
+﻿namespace JP_TS_15_MainConsoleProject
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        private string pin;
+        public string Pin
+        {
+            get { return pin; }
+            set
+            {
+                if (value.Length == 11)
+                {
+                    pin = value;
+                }
+            }
+        }
+
+        //კომპოზიცია
+        public Account Account { get; set; }
+    }
+}
