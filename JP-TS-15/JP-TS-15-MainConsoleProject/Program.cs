c9c97aee4635e6579f4309a114e0b573065c42b5
Nikoloz Chkhartishvili <nika.chkhartishvili7@gmail.com>
2023-05-20 19:55:16 +0400

    FindTheRichest method added

--- JP-TS-15/JP-TS-15-MainConsoleProject/Program.cs
+++ JP-TS-15/JP-TS-15-MainConsoleProject/Program.cs
@@ -12,7 +12,6 @@ User user1 = new()
         Currency = "GEL"
     }
 };
-
 User user2 = new()
 {
     FirstName = "Giorgi",
@@ -25,10 +24,71 @@ User user2 = new()
         Currency = "GEL"
     }
 };
+User user3 = new()
+{
+    FirstName = "Irakli",
+    LastName = "Giorgadze",
+    Pin = "12345678744",
+    Account = new Account()
+    {
+        AccountNumber = "1234767884512345678945",
+        Balance = 5,
+        Currency = "GEL"
+    }
+};
 
-//შექმენით User - ების მასივი სადაც შეინახავთ რამდნეიმე User-ს.
+
+User[] allUsers =
+{
+    user1,
+    user2,
+    user3,
+    new User
+    {
+        FirstName = "Guja",
+        LastName = "Gujiashvili",
+        Pin = "12345678944",
+        Account = new Account()
+        {
+            AccountNumber = "1234767884572345678945",
+            Balance = 5000,
+            Currency = "GEL"
+        }
+    }
+};
+User richestGuy = FindTheRichest(allUsers);
+
+File.WriteAllText("../../../richestUser.txt", $"{richestGuy.FirstName} {richestGuy.LastName} {richestGuy.Account.AccountNumber} {richestGuy.Account.Currency} {richestGuy.Account.Balance}");
+
+
+
+User FindTheRichest(User[] userCollection)
+{
+    User richestUser = userCollection[0];
+
+    for (int i = 0; i < userCollection.Length; i++)
+    {
+        if (userCollection[i].Account.Balance >= richestUser.Account.Balance)
+        {
+            richestUser = userCollection[i];
+        }
+    }
+
+    return richestUser;
+}
+
+
+
+
+
+
+
+//1.შექმენით User - ების მასივი სადაც შეინახავთ რამდნეიმე User-ს.
 //დაწერეთ ფუნქცია FindTheRichest მოძებნეთ ყველაზე მდიდარი User -ი თქვენს 
+
 //User ებს შორის და ჩაწერეთ ამ User-ის შესახებ სრული ინფორმაცია File-ში...
+
+
 
 
 
