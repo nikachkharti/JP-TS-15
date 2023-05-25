32064921a94c82175bffe71a0ba297544da49c35
Nikoloz Chkhartishvili <nika.chkhartishvili7@gmail.com>
2023-05-25 18:52:21 +0400

    Parse method head added

---
+++ JP-TS-15/JP-TS-15-MainConsoleProject/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿namespace JP_TS_15_MainConsoleProject
+{
+    public class Vehicle
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public byte Cylinder { get; set; }
+        public float Engine { get; set; }
+        public string Drive { get; set; }
+        public string Transmission { get; set; }
+        public byte City { get; set; }
+        public byte Combined { get; set; }
+        public byte Highway { get; set; }
+
+        public Vehicle Parse(string value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
