2d57a631a349cd5d859a658015efb7d464de8a73
Nikoloz Chkhartishvili <nika.chkhartishvili7@gmail.com>
2023-05-25 19:48:26 +0400

    Raw parse added

--- JP-TS-15/JP-TS-15-MainConsoleProject/Program.cs
+++ JP-TS-15/JP-TS-15-MainConsoleProject/Program.cs
@@ -1,2 +1,13 @@
 ﻿using JP_TS_15_MainConsoleProject;
 
+
+string[] data = File.ReadAllLines(@"../../../vehicles.csv");
+
+Vehicle[] vehicleArray = new Vehicle[data.Length];
+for (int i = 0; i < data.Length; i++)
+{
+    vehicleArray[i] = Vehicle.Parse(data[i]);
+}
+
+
+Console.ReadLine();
--- JP-TS-15/JP-TS-15-MainConsoleProject/Vehicle.cs
+++ JP-TS-15/JP-TS-15-MainConsoleProject/Vehicle.cs
@@ -12,9 +12,22 @@
         public byte Combined { get; set; }
         public byte Highway { get; set; }
 
-        public Vehicle Parse(string value)
+        public static Vehicle Parse(string value)
         {
-            throw new NotImplementedException();
+            string[] separatedValue = value.Split(',');
+
+            Vehicle result = new();
+            result.Make = separatedValue[0];
+            result.Model = separatedValue[1];
+            result.Cylinder = byte.Parse(separatedValue[2]);
+            result.Engine = float.Parse(separatedValue[3]);
+            result.Drive = separatedValue[4];
+            result.Transmission = separatedValue[5];
+            result.City = byte.Parse(separatedValue[6]);
+            result.Combined = byte.Parse(separatedValue[7]);
+            result.Highway = byte.Parse(separatedValue[8]);
+
+            return result;
         }
     }
 }
