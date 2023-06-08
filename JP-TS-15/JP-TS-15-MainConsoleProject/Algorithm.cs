ef841845142fdc00bb0bbbbb8664be8506e0c79c
Nikoloz Chkhartishvili <nika.chkhartishvili7@gmail.com>
2023-06-08 18:36:29 +0400

    Generics explained

--- JP-TS-15/JP-TS-15-MainConsoleProject/Algorithm.cs
+++ JP-TS-15/JP-TS-15-MainConsoleProject/Algorithm.cs
@@ -1,5 +1,9 @@
 ﻿namespace JP_TS_15_MainConsoleProject
 {
+    //Action --> იმახსოვრებს ისეთ ფუნქციას რომელიც აბრუნებს void_ს
+    //Func --> იმახსოვრებს ისეთ ფუნქციას რომელსაც მისაღებ და დასაბრუნებელ პარამეტრებს ვურჩევთ ჩვენ
+    //Predicate --> იმახსოვრებს ისეთ ფუნქციას რომელსაც მისაღებ პარამეტრებს ვურჩევთ ჩვენ, დასაბრუნებელი პარამეტრი ყოველითვის Boolean -ია
+
     public static class Algorithm
     {
         public static Vehicle[] Take(Vehicle[] vehicles, int quantity)
@@ -27,20 +31,6 @@
 
             return vehicleArray;
         }
-        public static List<Vehicle> FindAll(Vehicle[] vehicles, string make)
-        {
-            List<Vehicle> result = new();
-
-            for (int i = 0; i < vehicles.Length; i++)
-            {
-                if (vehicles[i].Make.Contains(make))
-                {
-                    result.Add(vehicles[i]);
-                }
-            }
-
-            return result;
-        }
         public static Vehicle[] OrderBy(Vehicle[] vehicleArray)
         {
             for (int i = 0; i < vehicleArray.Length - 1; i++)
@@ -59,5 +49,58 @@
 
             return vehicleArray;
         }
+
+        public static List<Vehicle> FindAll(Vehicle[] vehicles, string make)
+        {
+            List<Vehicle> result = new();
+
+            for (int i = 0; i < vehicles.Length; i++)
+            {
+                if (vehicles[i].Make.Contains(make))
+                {
+                    result.Add(vehicles[i]);
+                }
+            }
+
+            return result;
+        }
+        public static int FirstOrDefault(int[] collection, Func<int, bool> comparer)
+        {
+            for (int i = 0; i < collection.Length; i++)
+            {
+                if (comparer(collection[i]))
+                {
+                    return collection[i];
+                }
+            }
+
+            return default;
+        }
+
+
+        public static int FindIndex<T>(T[] collection, Predicate<T> predicate)
+        {
+            for (int i = 0; i < collection.Length; i++)
+            {
+                if (predicate(collection[i]))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+        public static int FindLastIndex<T>(T[] collection, Func<T, bool> predicate)
+        {
+            for (int i = collection.Length - 1; i >= 0; i--)
+            {
+                if (predicate(collection[i]))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
     }
 }
--- JP-TS-15/JP-TS-15-MainConsoleProject/Program.cs
+++ JP-TS-15/JP-TS-15-MainConsoleProject/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello World");
+﻿using JP_TS_15_MainConsoleProject;
+
+int[] intArray = { 2, 1, 2, 3, 4, 11, -1, -2, 11 };
+string[] names = { "Nika", "Giorgi", "Daviti", "Daviti" };
+
+
+
+var result = Algorithm.FindLastIndex<string>(names, x => x == "Lasha");
+
+
+
+
+Console.ReadLine();
