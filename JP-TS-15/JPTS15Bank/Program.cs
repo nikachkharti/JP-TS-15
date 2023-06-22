260f645e6ae692b4d8dc30f47faa8dd4a8248418
Nikoloz Chkhartishvili <nika.chkhartishvili7@gmail.com>
2023-06-22 18:49:57 +0400

    Parse method added

--- JP-TS-15/JPTS15Bank/Program.cs
+++ JP-TS-15/JPTS15Bank/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using JPTS15Bank.Repositories.Implementations;
+
+CustomerRepository repo = new();
+
+
+
+Console.ReadKey();
--- JP-TS-15/JPTS15Bank/Repositories/Implementations/CustomerRepository.cs
+++ JP-TS-15/JPTS15Bank/Repositories/Implementations/CustomerRepository.cs
@@ -5,6 +5,18 @@ namespace JPTS15Bank.Repositories.Implementations
 {
     public class CustomerRepository : ICustomerRepository
     {
+        private const string _filePath = @"../../../Data/Customers.csv";
+        private List<Customer> _customers = new();
+
+        public CustomerRepository()
+        {
+            _customers = File
+                .ReadAllLines(_filePath)
+                .Skip(1)
+                .Select(x => x.ToCustomer())
+                .ToList();
+        }
+
         public void AddCustomer(Customer model)
         {
             throw new NotImplementedException();
@@ -15,4 +27,26 @@ namespace JPTS15Bank.Repositories.Implementations
             throw new NotImplementedException();
         }
     }
+
+
+    internal static class CustomerExtension
+    {
+        internal static Customer ToCustomer(this string input)
+        {
+            string[] csvInput = input.Split(',');
+
+            Customer result = new()
+            {
+                Id = int.Parse(csvInput[0]),
+                Name = csvInput[1],
+                IdentityNumber = csvInput[2],
+                PhoneNumber = csvInput[3],
+                Email = csvInput[4],
+                Type = (CustomerType)Enum.Parse(typeof(CustomerType), csvInput[5])
+            };
+
+            return result;
+        }
+    }
+
 }
