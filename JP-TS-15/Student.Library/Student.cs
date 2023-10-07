a95fea0fc729ba600c03030752d73be4c64c9237
nikachkharti <nika.chkhartishvili7@gmail.com>
2023-10-07 19:01:18 +0400

    Students project started

---
+++ JP-TS-15/Student.Library/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace Student.Library
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string? Pin { get; set; }
+    }
+}
