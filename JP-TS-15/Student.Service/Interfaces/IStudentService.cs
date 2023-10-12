be9de796a3535c60e853140d57e59b58dd29be06
nikachkharti <nika.chkhartishvili7@gmail.com>
2023-10-12 19:21:25 +0400

    Task added

--- JP-TS-15/Student.Service/Interfaces/IStudentService.cs
+++ JP-TS-15/Student.Service/Interfaces/IStudentService.cs
@@ -5,5 +5,6 @@ namespace Student.Service.Interfaces
     public interface IStudentService
     {
         List<StudentModel> GetAllStudents();
+        //TODO --- დაწერეთ GetSingleStudent (int id) რომელიც მონაცემთა ბაზიდან წამოიღებს ერთ კონკრეტულ სტუდენტს
     }
 }
--- JP-TS-15/Student.Service/Interfaces/ITeacherService.cs
+++ JP-TS-15/Student.Service/Interfaces/ITeacherService.cs
@@ -5,5 +5,7 @@ namespace Student.Service.Interfaces
     public interface ITeacherService
     {
         List<TeacherModel> GetAllTeachers();
+
+        //TODO --- დაწერეთ GetSingleTeacher(int id) რომელიც მონაცემთა ბაზიდან წამოიღებს ერთ კონკრეტულ მასწავლებელს
     }
 }
