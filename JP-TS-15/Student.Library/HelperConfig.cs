13bf6ec4af5d10e8ae33e2889fc0aa8eb16ae95b
nikachkharti <nika.chkhartishvili7@gmail.com>
2023-10-28 18:27:44 +0400

    Project deleted

--- JP-TS-15/Student.Library/HelperConfig.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Student.Library
-{
-    public static class HelperConfig
-    {
-        public static string? ConnectionString { get; } = @"Server=T-21-01\SQLEXPRESS;Database=JPTS15;Trusted_Connection=True;TrustServerCertificate=True";
-    }
-}
