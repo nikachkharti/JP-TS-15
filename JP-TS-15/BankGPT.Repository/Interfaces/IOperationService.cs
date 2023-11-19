b372528a3d0e30186ded480df28352744c122baa
Admin <Admin@T-25-01>
2023-11-19 17:12:04 +0400

    Operations task added

---
+++ JP-TS-15/BankGPT.Repository/Interfaces/IOperationService.cs
@@ -0,0 +1,13 @@
+﻿using BankGPT.Library;
+
+namespace BankGPT.Repository.Interfaces
+{
+    public interface IOperationService
+    {
+        Task<List<OperationModel>> GetAllOperationsAsync();
+        Task<List<OperationModel>> GetAllOperationsForCustomerAsync(int customerId);
+        Task<List<OperationModel>> GetAllOperationsForAccountAsync(int accountId);
+        Task CreateOperationAsync(OperationModel model);
+        Task DeleteOperationAsync(int operationId);
+    }
+}
