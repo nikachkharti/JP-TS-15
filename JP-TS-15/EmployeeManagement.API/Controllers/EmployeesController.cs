2a4c47593ac7f109420b94f9e4e544dc7cb932b0
visual studio <vs@itstep.ge>
2023-11-26 16:41:24 +0400

    TEST

---
+++ JP-TS-15/EmployeeManagement.API/Controllers/EmployeesController.cs
@@ -0,0 +1,23 @@
+﻿using EmployeeManagement.API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.API.Controllers
+{
+    [ApiController]
+    [Route("api/employees")]
+    public class EmployeesController : ControllerBase
+    {
+        [HttpGet]
+        public List<Employee> GetEmployees()
+        {
+            var allEmployees = EmployeeStore.GetAllEmployees();
+            return allEmployees;
+        }
+
+        [HttpGet]
+        public Employee GetEmployee()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ JP-TS-15/EmployeeManagement.API/Data/EmployeeStore.cs
@@ -0,0 +1,17 @@
+﻿namespace EmployeeManagement.API.Data
+{
+    public static class EmployeeStore
+    {
+        public static List<Employee> GetAllEmployees()
+        {
+            return new List<Employee>()
+            {
+                new Employee(){Id = 1, FirstName = "Nodari",LastName = "Eqvtimishvili"},
+                new Employee(){Id = 2, FirstName = "Guka",LastName = "Shinjikashvili"},
+                new Employee(){Id = 3, FirstName = "Aleksandre",LastName = "Iosava"},
+                new Employee(){Id = 4, FirstName = "Tako",LastName = "Makhatadze"},
+                new Employee(){Id = 5, FirstName = "Nika",LastName = "Kuprashvili"}
+            };
+        }
+    }
+}
