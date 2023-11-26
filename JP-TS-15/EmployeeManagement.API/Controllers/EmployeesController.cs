629091ba93a58a2e13db2dbbde25f6f715aa2ce3
visual studio <vs@itstep.ge>
2023-11-26 17:59:26 +0400

    Companes task added

--- JP-TS-15/EmployeeManagement.API/Controllers/EmployeesController.cs
+++ JP-TS-15/EmployeeManagement.API/Controllers/EmployeesController.cs
@@ -8,23 +8,84 @@ namespace EmployeeManagement.API.Controllers
     public class EmployeesController : ControllerBase
     {
         [HttpGet]
-        public List<Employee> GetEmployees()
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<List<Employee>> GetEmployees()
         {
             var result = EmployeeStore.Employees;
-            return result;
+            return Ok(result);
         }
 
         [HttpGet("{id:int}")]
-        public Employee GetEmployee(int id)
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<Employee> GetEmployee(int id)
         {
+            if (id <= 0)
+                return BadRequest();
+
             var result = EmployeeStore.Employees.FirstOrDefault(x => x.Id == id);
-            return result;
+
+            if (result == null)
+                return NotFound();
+
+            return Ok(result);
         }
 
         [HttpPost]
-        public void AddNewEmployee(Employee model)
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public ActionResult AddNewEmployee(Employee model)
         {
+            if (model == null)
+                return BadRequest();
+
+            var newId = EmployeeStore.Employees.Max(x => x.Id) + 1;
+            model.Id = newId;
+
             EmployeeStore.Employees.Add(model);
+            return Created(string.Empty, model);
         }
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult UpdateEmployee(int id, Employee model)
+        {
+            if (id <= 0 || model.Id != id)
+                return BadRequest();
+
+            var result = EmployeeStore.Employees.FirstOrDefault(x => x.Id == id);
+
+            if (result == null)
+                return NotFound();
+
+            result.FirstName = model.FirstName;
+            result.LastName = model.LastName;
+
+            return Ok();
+        }
+
+
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public ActionResult<Employee> DeleteEmployee(int id)
+        {
+            if (id <= 0)
+                return BadRequest();
+
+            var result = EmployeeStore.Employees.FirstOrDefault(x => x.Id == id);
+
+            if (result == null)
+                return NotFound();
+
+            EmployeeStore.Employees.Remove(result);
+
+            return NoContent();
+        }
+
     }
 }
---
+++ JP-TS-15/EmployeeManagement.API/Models/Company.cs
@@ -0,0 +1,9 @@
+﻿namespace EmployeeManagement.API.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
