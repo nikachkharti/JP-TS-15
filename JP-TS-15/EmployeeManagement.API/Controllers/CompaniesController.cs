f6ed9c3554245b24c2d005d00f7427bddfe7e34c
visual studio <vs@itstep.ge>
2023-12-03 17:22:45 +0400

    Companies CRUD provided

--- JP-TS-15/EmployeeManagement.API/Controllers/CompaniesController.cs
+++ JP-TS-15/EmployeeManagement.API/Controllers/CompaniesController.cs
@@ -1,36 +1,25 @@
 ﻿using EmployeeManagement.API.Data;
+using EmployeeManagement.API.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeManagement.API.Controllers
 {
+    [Route("api/companies")]
     [ApiController]
-    [Route("api/employees")]
-    public class EmployeesController : ControllerBase
+    public class CompaniesController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
-        public EmployeesController(ApplicationDbContext context)
+        public CompaniesController(ApplicationDbContext context)
         {
             _context = context;
         }
 
         [HttpGet]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        public ActionResult<List<Employee>> GetEmployees()
-        {
-            var result = _context.Employees.ToList();
-            return Ok(result);
-        }
-
-        [HttpGet("{id:int}")]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public ActionResult<Employee> GetEmployee([FromRoute] int id)
+        public ActionResult<List<Company>> GetCompanies()
         {
-            if (id <= 0)
-                return BadRequest();
-
-            var result = _context.Employees.FirstOrDefault(x => x.Id == id);
+            var result = _context.Companies.ToList();
 
             if (result == null)
                 return NotFound();
@@ -38,54 +27,72 @@ namespace EmployeeManagement.API.Controllers
             return Ok(result);
         }
 
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<Company> GetCompany([FromRoute] int id)
+        {
+            if (id <= 0)
+                return BadRequest();
+
+            var result = _context.Companies.FirstOrDefault(x => x.Id == id);
+
+            if (result == null)
+                return NotFound();
+
+            return Ok(result);
+        }
+
+
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status201Created)]
-        public ActionResult AddNewEmployee([FromBody] Employee model)
+        public ActionResult AddNewCompany([FromBody] Company model)
         {
             if (model == null)
                 return BadRequest();
 
-            _context.Employees.Add(model);
+            _context.Companies.Add(model);
             _context.SaveChanges();
 
             return Created(string.Empty, model);
         }
 
-        [HttpPut]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        public ActionResult UpdateEmployee([FromBody] Employee model)
-        {
-            if (model.Id <= 0 || model == null)
-                return BadRequest();
-
-            _context.Employees.Update(model);
-            _context.SaveChanges();
-
-            return Ok();
-        }
-
-        [HttpDelete]
+        [HttpDelete("{id:int}")]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
-        public ActionResult<Employee> DeleteEmployee([FromRoute] int id)
+        public ActionResult DeleteCompany([FromRoute] int id)
         {
             if (id <= 0)
                 return BadRequest();
 
-            var result = _context.Employees.FirstOrDefault(x => x.Id == id);
+            var result = _context.Companies.FirstOrDefault(x => x.Id == id);
 
             if (result == null)
                 return NotFound();
 
-            _context.Employees.Remove(result);
+            _context.Companies.Remove(result);
             _context.SaveChanges();
 
             return NoContent();
         }
 
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult UpdateCompany([FromBody] Company model)
+        {
+            if (model.Id <= 0 || model == null)
+                return BadRequest();
+
+            _context.Companies.Update(model);
+            _context.SaveChanges();
+
+            return Ok();
+        }
     }
 }
